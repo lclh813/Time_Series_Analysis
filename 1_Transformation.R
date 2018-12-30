9684943500ca81a8cf8eea0ade6a02ff85d6ab14
lclh813 <liaokelan@gmail.com>
2018-12-30 23:38:23 +0800

    Delete 1_Transformation.R

--- 1_Transformation.R
+++
@@ -1,9 +0,0 @@
-# Import dataset.
-data <- read.table("G:\\Data.csv")
-Zt <- data$V1
-
-require(forecast)
-# Calculate the exponent and round down to the 2nd digit.
-lambda <- round(BoxCox.lambda(Zt),digits=2)
-# Apply variance stabilizing transformation.
-Zt_trans <- Zt^(lambda)
