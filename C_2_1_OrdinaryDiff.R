c97737dba75dff8e6448eb42d91434c204b1e6dc
lclh813 <liaokelan@gmail.com>
2018-12-31 12:30:12 +0800

    Add files via upload

---
+++ C_2_1_OrdinaryDiff.R
@@ -0,0 +1,6 @@
+#### Step 2. Identfy the Order of d
+#### Step 2.1. Ordinary Differencing
+# Take a simple first difference of the transformed series.
+dYt <- diff(Yt)
+# Examine ACF and PACF after differencing.
+acf(dYt,lag=20); pacf(dYt,lag=20)
