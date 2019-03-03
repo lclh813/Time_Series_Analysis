9de9a076e890d49dff3a063570663607aae8fef5
lclh813 <liaokelan@gmail.com>
2019-03-03 19:28:29 +0800

    Delete C_3_3_M5.R

--- Code/C_3_3_M5.R
+++
@@ -1,7 +0,0 @@
-#### Step 3. Estimate the Orders of p and q ####
-#### Option 3.3. Automatic ARIMA Modelling ####
-m5 <- auto.arima(dYt_s)
-# Examine ACF and PACF of Model 4's residuals.
-acf(m5$residuals,lag=20); pacf(m5$residuals,lag=20)
-# Examine Model 5's coefficients and standard errors.
-m5
